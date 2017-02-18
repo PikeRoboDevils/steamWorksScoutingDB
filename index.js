cc4c17503c9175e0a8bcdae13d274200531e0c79
Cameron Forsythe <cameronForsythe@Camerons-MacBook-Pro.local>
2017-02-18 13:30:52 -0500

    Bluetooth database stuff

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('scouting.db');
 
 console.log('App started');
 
@@ -33,6 +35,32 @@ bleno.on('advertisingStart', function(error){
               onWriteRequest : function(data, offset, withoutResponse, callback){
                 // var attendee = JSON.parse(data.toString());
                 var match = JSON.parse(data.toString());
+
+                db.run("INSERT INTO event VALUES ($matchNumber, $teamNumber, $scout, $autoHighFuel, $autoLowFuel, $autoKpa, $autoRotors, $autoGears, $baseline, $autoScore, $teleOpHighFuel, $teleOpLowFuel, $teleOpKpa, $teleOpRotors, $teleOpGears, $teleOpScore, $climb, $redCard, $yellowCard, $foul, $tech, $malfunction, $totalScore, $outcome)",
+                {$matchNumber: match.matchNumber,
+                $teamNumber: match.teamNumber,
+                $scout: match.scout,
+                $autoHighFuel: match.results.autoResults.highFuel,
+                $autoLowFuel: match.results.autoResults.lowFuel,
+                $autoKpa: match.results.autoResults.kpa,
+                $autoRotors: match.results.autoResults.rotorTotal,
+                $autoGears: match.results.autoResults.gearTotal,
+                $baseline: match.results.autoResults.baseLine,
+                $autoScore: match.results.autoResults.totalAutoPoints,
+                $teleOpHighFuel: match.results.teleOpResults.highFuel,
+                $teleOpLowFuel: match.results.teleOpResults.lowFuel,
+                $teleOpKpa: match.results.teleOpResults.kpa,
+                $teleOpRotors: match.results.teleOpResults.rotorTotal,
+                $teleOpGears: match.results.teleOpResults.gearTotal,
+                $teleOpScore: match.results.teleOpResults.totalTelopPoints,
+                $climb: match.results.teleOpResults.climb,
+                $redCard: match.results.endGameResults.red,
+                $yellowCard: match.results.endGameResults.yellow,
+                $foul: match.results.endGameResults.foul,
+                $tech: match.results.endGameResults.tech,
+                $malfunction: match.results.endGameResults.malfunction,
+                $totalScore: match.results.endGameResults.totalPoints,
+                $outcome: match.results.endGameResults.outcome});
 
 
                 // attendee.time_entered = Date.now();
