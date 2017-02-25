e17c55f928a9781d33551b2d066d59ffbfdda24a
Davis <derekdavis2013@gmail.com>
2017-02-25 13:12:37 -0500

    noble test

---
+++ noble.js
@@ -0,0 +1,13 @@
+var noble = require('noble');
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+    console.log('found peripheral:', peripheral.advertisement);
+});
