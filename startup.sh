654e0d6b94bee183d585450ced489af844ecd29e
Davis <derekdavis2013@gmail.com>
2017-03-13 19:07:51 -0400

    pop new terminal

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xterm -hold -e "mongod --dbpath=/home/pi/data --port=9000"
+xterm -hold -e "mongod --dbpath=/home/pi/data --port=9000";
 
-sudo node index.js;
+xterm -hold -e cd steamWorksScoutingDB; sudo node index.js;
