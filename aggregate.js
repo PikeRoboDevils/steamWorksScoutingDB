00d1fc859494945433b793852872c78c6909e288
derekdavis2013 <derekdavis2013@gmail.com>
2018-03-17 18:32:15 +0000

    mishawaa state

--- aggregate.js
+++ aggregate.js
@@ -1,15 +1,19 @@
 use steamworks;
 
-var result = db.mishawaka.aggregate([
+var result = db.tester.aggregate([
+
     {
         $project:
             {
                 "teamNumber": 1,
-                "autoScore.total": 1,
+                //"autoScore.total": 1,
+                "autoScore.cubes": 1,
+                "autoScore.autoRun": 1,
+                "teleScore.breakdown": 1,
                 "teleScore.vaultPoints": 1,
                 "teleScore.levitation": 1,
                 "teleScore.cubes": 1,
-                "teleScore.total": 1,
+                //"teleScore.total": 1,
                 "teleScore.foul": 1,
                 "teleScore.parking": 1,
                 "teleScore.playStyle": 1,
@@ -24,16 +28,20 @@ var result = db.mishawaka.aggregate([
         $group:
             {
                 "_id": "$teamNumber",
-                "teleFoul": {"$sum": "$teleScore.foul"},
-                "teleVault": {"$sum": "$teleScore.vaultPoints"},
-                "teleParking": {"$sum": "$teleScore.parking"},
-                "autoPlace": {"$sum": "$autoScore.placement"}, 
-                "teleLevi": {"$avg": "$teleScore.levitation"},
-                "telePlay": {"$sum": "$teleScore.playStyle"},
-                "teleTotal": {"$sum": "$teleScore.total"},
-                "teleCubes": {"$sum": "$teleScore.cubes"},
-                "teleAvgCubes": {"$avg": "teleScore.cubes"},  
-                "autoTotal": {"$sum": "$autoScore.total"},
+                "avgAutoCubes": {"$avg": "$autoScore.cubes"},
+                "fouls": {"$sum": "$teleScore.foul"},
+                "vaultPoints": {"$sum": "$teleScore.vaultPoints"},
+                "avgAutoRun": {"$avg": "$autoScore.autoRun"},
+                "breakdowns": {"$sum": "$teleScore.breakdown"},
+                //"teleParking": {"$sum": "$teleScore.parking"},
+                "autoPlacement": {"$sum": "$autoScore.placement"}, 
+                "levitate": {"$sum": "$teleScore.levitation"},
+                "playStyle": {"$sum": "$teleScore.playStyle"},
+                //"teleTotal": {"$sum": "$teleScore.total"},
+                //"teleCubes": {"$sum": "$teleScore.cubes"},
+                "avgAutoCubes": {"$avg": "$autoScore.cubes"},
+                "avgTeleCubes": {"$avg": "teleScore.cubes"},  
+                //"autoTotal": {"$sum": "$autoScore.total"},
                 "attemptedClimbs": {"$sum": {"$cond": ["$teleScore.climbAttempt", 1, 0]}},
                 "successfulClimbs": {"$sum": {"$cond": ["$teleScore.climbSuccess", 1, 0]}},
                 "matches": {"$sum": 1}
